138b6418b172ae4950245f5a024db9bcc74b8fd7
andreafrancia <andreafrancia@localhost>
2009-10-04 18:43:58 +0200

    bash: svn-add-to-ignore-list: No such file or directory

---
+++ svn-ignore-tests.bash
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+function test-add-first-ignore-in-cur-dir() {
+  setup-test-repo-and-wc
+  
+  (
+    cd wc
+    svn-ignore-add path-to-ignore
+  )
+  
+  assertEquals "path-to-ignore" "$(read-ignore-prop wc)"
+}
+
+function test-add-first-ignore-in-other-dir() {
+  setup-test-repo-and-wc
+  
+  svn-ignore-add wc/path-to-ignore
+  
+  assertEquals "path-to-ignore" "$(read-ignore-prop wc)"
+}
+
+function test-append-second-ignore() {
+  setup-test-repo-and-wc
+  
+  (
+    cd wc
+    svn-ignore-add first-path-to-ignore
+    svn-ignore-add second-path-to-ignore
+  )
+  
+  assertEquals "first-path-to-ignore
+second-path-to-ignore" "$(read-ignore-prop wc)"
+}
+
+function test-append-second-ignore-on-other-dir() {
+  setup-test-repo-and-wc
+  
+  svn-ignore-add wc/first-path-to-ignore
+  svn-ignore-add wc/second-path-to-ignore
+  
+  assertEquals "first-path-to-ignore
+second-path-to-ignore" "$(read-ignore-prop wc)"
+}
+
+function test-get-current-ignore-list() {
+  setup-test-repo-and-wc
+  (
+    cd wc
+    svn-ignore-add my-path-to-ignore
+  )
+  
+  assertEquals "my-path-to-ignore" "$(svn-get-ignore-list wc)"
+}
+
+
+function test-that-repo-is-clean() {
+  setup-test-repo-and-wc
+
+  local ignore="$(read-ignore-prop wc)"
+  
+  assertEquals "" "$ignore"
+}
+
+function read-ignore-prop() {
+  local wc="$1"
+  svn propget svn:ignore "$wc"
+}
+
+function setup-test-repo-and-wc() {
+  rm -Rf repo
+  rm -Rf wc
+  svnadmin create repo >/dev/null
+  svn co file://$(pwd)/repo wc >/dev/null
+}
+
---
+++ svn-ignore.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+function svn-ignore-add() {
+  local path_to_ignore="$1"
+  local wc="$(dirname "$path_to_ignore")"
+  local new_ignore_list="$(concat-to-current-ignore-list "$wc" "$(basename "$path_to_ignore")" )"
+  update-ignore-list-on "$new_ignore_list" "$wc"
+}
+
+function update-ignore-list-on () {
+  local ignore_list="$1"
+  local working_copy="$2"
+  svn propset svn:ignore "$ignore_list" "$working_copy" >/dev/null
+}
+
+function concat-to-current-ignore-list() {
+  local working_dir="$1"
+  local new_path_to_ignore="$2"
+
+  {
+    svn-get-ignore-list "$working_dir"
+    echo "$new_path_to_ignore"
+  } | discard-empty-lines
+}
+
+function svn-get-ignore-list() {
+  local wc_path="$1"
+  svn propget svn:ignore "$wc_path"
+}
+
+function discard-empty-lines() {
+  sed '/^\s*$/d'
+}
+
+
